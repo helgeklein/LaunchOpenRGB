d2b695f513c67688557812cc38ddf9cd037794a4
Helge Klein <helge@vastlimits.com>
2021-10-23 21:54:02 +0200

    Initial commit

---
+++ LaunchOpenRGB.ps1
@@ -0,0 +1,32 @@
+#
+# Setttings: configure
+#
+$profileFile = "NAME OF YOUR .ORP FILE"
+$backupFolder = "PATH TO BACKUP DIRECTORY ENDING WITH A BACKSLASH\" + $env:computername
+$openRGBExe = "PATH TO OpenRGB.exe"
+
+#
+# Variables: do not modify unless you know what you're doing
+#
+$profileSourceFolder = $env:appdata + "\OpenRGB"
+
+#
+# Script start
+#
+
+# Wait
+Start-Sleep -Seconds 10
+
+# Copy the profile file
+$source = $profileSourceFolder + "\" + $profileFile
+Copy-Item -Path $source -Destination $backupFolder -Force
+
+# Launch the tool
+$profileFileEscaped =  '"' + $profileFile + '"'
+& $openRGBExe --startminimized --profile $profileFileEscaped
+
+# Wait
+Start-Sleep -Seconds 10
+
+# Kill OpenRGB
+Stop-Process -Name OpenRGB
---
+++ README.md
@@ -0,0 +1,10 @@
+# LaunchOpenRGB
+
+- Launches OpenRGB with a profile so it applies your keyboard lighting settings.
+- Stops OpenRGB after a safe-enough time when it's not needed any more.
+
+Goodie: is copies your OpenRGB profile file to a backup directory
+
+Works best when run as a scheduled task.
+
+More information: 
